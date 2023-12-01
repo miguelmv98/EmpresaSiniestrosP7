069e9d48a38e598f664f2cd051741a193ba053e9
Miguel Monje <mmv776@alumnos.unican.es>
2023-12-01 08:43:52 +0100

    New Repo From EmpresaSiniestrosP4

--- src/formularios/VentanaErrorDatos.java
+++ src/formularios/VentanaErrorDatos.java
@@ -11,7 +11,7 @@ import javax.swing.SwingConstants;
 
 public class VentanaErrorDatos extends JFrame {
 
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 2L;
 	JLabel lblMensajeError;
 
 	/**
